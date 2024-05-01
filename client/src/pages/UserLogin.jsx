841e4d39543ff3ab16a296352f57230dc7064ab2
Sathwik Pendem <97405473+sathwik0003@users.noreply.github.com>
2024-05-02 04:50:22 +0530

    Update UserLogin.jsx

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -50,7 +50,7 @@ const UserLogin = () => {
     }
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3002/api/user/login', {
+      const response = await fetch('https://petverse-3.onrender.com/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
