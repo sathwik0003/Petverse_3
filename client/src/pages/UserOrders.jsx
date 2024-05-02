e3c6897b509995044ef2421f534396770f1f96f7
Sathwik Pendem <97405473+sathwik0003@users.noreply.github.com>
2024-05-02 06:18:19 +0530

    Update UserOrders.jsx

--- client/src/pages/UserOrders.jsx
+++ client/src/pages/UserOrders.jsx
@@ -51,7 +51,7 @@ const UserOrders = ({ userid }) => {
         const { id, title, description, pet_category, product_category, available, price, image, brandcode } = product;
         setCart((prevCart) => [...prevCart, product]);
         setShowToast(true);
-        const response = await fetch(https://petverse-3.onrender.com/api/cart/${userid}`, {
+        const response = await fetch(`https://petverse-3.onrender.com/api/cart/${userid}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
